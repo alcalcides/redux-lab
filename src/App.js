caae4b02ba3b3515a08351616d06712e52b5839d
Alcides Neto <=>
2021-10-22 13:38:58 -0300

    Exposes storage for the whole application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { Provider } from 'react-redux'
+
 import './App.css'
 
 import { Counter } from './components/Counter'
 import { Dashboard } from './components/Dashboard'
 import { Form } from './components/Form'
+import store from './store'
 
 export default function App() {
   return (
-    <div>
-      <h1>Teste redux</h1>
-      <Counter />
-      <Form />
-      <Dashboard />
+    <div className='App'>
+      <Provider store={store}>
+        <h1>Teste redux</h1>
+        <Counter />
+        <Form />
+        <Dashboard />
+      </Provider>
     </div>
   )
 }
